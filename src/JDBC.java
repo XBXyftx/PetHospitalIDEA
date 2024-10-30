548f09ecfc2a52c960190c4591421cba6e4082f7
一方通行 <145579105+XBXyftx@users.noreply.github.com>
2024-10-30 20:39:39 +0800

    a

---
+++ src/JDBC.java
@@ -0,0 +1,35 @@
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class JDBC {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        //加载驱动
+        Class.forName("com.mysql.jdbc.Driver");
+
+        //用户信息和url
+        String url = "jdbc:mysql://localhost:3306/db01?useUnicode=true&characterEncoding=utf8&useSSL=true";
+        String user = "root";
+        String password = "12341234";
+
+        //连接数据库
+        Connection connection = DriverManager.getConnection(url, user, password);
+
+        //执行SQL的对象
+        Statement statement = connection.createStatement();
+
+        //执行SQL的对象
+        String sql = "select username from tb_user where id=1";
+
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()){
+            System.out.println(resultSet.getString("username"));
+        }
+
+        //释放资源
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+}
